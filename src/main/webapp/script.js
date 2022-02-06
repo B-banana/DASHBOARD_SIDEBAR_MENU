c8a3941171379e51b3bca862c1a338a233b8ab76
Lee GyeongCheol <niceburuma88@gmail.com>
2022-02-06 22:47:12 +0900

    22.02.06
    수정. 미완성 완료

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,4 +1,4 @@
-const body = document.querySelector("body"),
+ const body = document.querySelector("body"),
 	sidebar = body.querySelector(".sidebar"),
 	toggle = body.querySelector(".toggle"),
 	searchBtn = body.querySelector(".search-box"),
@@ -8,3 +8,18 @@ const body = document.querySelector("body"),
 modeSwtich.addEventListener("click", () => {
 	body.classList.toggle("dark");
 })
+
+searchBtn.addEventListener("click", () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+    
+    if (body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    } else {
+        modeText.innerText = "Dark mode";
+        
+    }
+});
